56cb82455462e015dfc8ab643a513d7e2ee4e036
Jacob Puckettgit config --global user.email jjp10015@gmail.com <jjp10015@gmail.com>
2023-03-02 13:58:52 -0700

    updated rollbar

--- server/server.js
+++ server/server.js
@@ -43,16 +43,16 @@ app.get('/js', (req,res) => {
     rollbar.error('black pearl does zoom')
 })
 
-function doIt() {
-    try {
-        return 1;
-    } finally {
-        rollbar.info("did it work")
-        return 2;
+// function doIt() {
+//     try {
+//         return 1;
+//     } finally {
+//         rollbar.info("did it work")
+//         return 2;
        
-    }
-}
+//     }
+// }
 
-doIt();
+// doIt();
 
 app.listen(4000, console.log(`server running on 4000`))
